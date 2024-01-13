e0ae107e60b59cb0c59bf17004839dd17b975dd3
Youssef Sherif <youssef98sherif@gmail.com>
2024-01-13 18:38:35 +0200

    add show to the users controller

---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should show one user' do
+    
+  end
+end
